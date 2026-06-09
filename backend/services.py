f7dd492b06c07e56ea175c51fbaddd3d54a76451
unknown <kalaniparapitiya@gmail.com>
2026-06-09 14:19:51 +0200

    Fixed issues in main summery and time

--- backend/services.py
+++ backend/services.py
@@ -60,7 +60,7 @@ def extract_search_ai_summary(segments, key):
             "topic": "Neural Networks",
             "lecture_summary": "Introduction to neural network layers. The lecture explains neural network layers and activation functions. Additionally, neural networks are inspired by the human brain and are used in image recognition and AI systems. The AI explanation adds practical applications and deeper conceptual understanding beyond the lecture.",
             "keywords": ["neurons", "layers"],
-            "timestamp": 120,
+            "start_timestamp": 120,
             "endtime": 180,
             "resources": [
             {{
@@ -125,7 +125,7 @@ Analyze this lecture transcript.
         "topic": "Neural Networks",
         "summary": "Introduction to neural networks",
         "keywords": ["neurons", "layers"],
-        "timestamp": 120,
+        "start_timestamp": 120,
         "endtime: 150,
         transcript : "I can't share it in the PDF, yeah, absolutely. If you want to have these slides here, but this is not included yet because just like an informal Q&A..."
       }}
@@ -182,7 +182,7 @@ def extract_topics_with_ai(segments):
         "topic": "Neural Networks",
         "summary": "Introduction to neural networks",
         "keywords": ["neurons", "layers"],
-        "timestamp": 120,
+        "start_timestamp": 120,
         "endtime: 150,
         transcript : "I can't share it in the PDF, yeah, absolutely. If you want to have these slides here, but this is not included yet because just like an informal Q&A..."
       }}
